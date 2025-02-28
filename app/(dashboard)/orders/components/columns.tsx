f37f0996f57e9ab112ce26b318718c24d143aafb
train <train@ttt.com>
2025-02-28 11:09:07 +0800

    u

--- app/(dashboard)/orders/components/columns.tsx
+++ app/(dashboard)/orders/components/columns.tsx
@@ -1,16 +1,16 @@
-"use client"
+"use client";
 
-import { ColumnDef } from "@tanstack/react-table"
+import { ColumnDef } from "@tanstack/react-table";
 
-import { Badge } from "@/components/ui/badge"
-import { Checkbox } from "@/components/ui/checkbox"
+import { Badge } from "@/components/ui/badge";
+import { Checkbox } from "@/components/ui/checkbox";
 
-import { DataTableColumnHeader } from "@/components/biz/data-table/data-table-column-header"
-import { DataTableRowActions } from "@/components/biz/data-table/data-table-row-actions"
-import Link from "next/link"
-import CustomImage from "@/components/biz/custom-image"
-import { format } from "date-fns"
-import { TColumn } from "@/components/biz/data-table"
+import { DataTableColumnHeader } from "@/components/biz/data-table/data-table-column-header";
+import { DataTableRowActions } from "@/components/biz/data-table/data-table-row-actions";
+import Link from "next/link";
+import CustomImage from "@/components/biz/custom-image";
+import { format } from "date-fns";
+import { TColumn } from "@/components/biz/data-table";
 export type OrderItem = {
   id: string;
   width?: number;
@@ -21,9 +21,10 @@ export type OrderItem = {
   total_price: string;
   shipping_address: any;
   skuStatus?: any;
-  products?:any
-}
-export const columns: TColumn<OrderItem,unknown>[] = [
+  skuData?: any;
+  products?: any;
+};
+export const columns: TColumn<OrderItem, unknown>[] = [
   {
     id: "select",
     header: ({ table }) => (
@@ -40,13 +41,15 @@ export const columns: TColumn<OrderItem,unknown>[] = [
     cell: ({ row }) => {
       return (
         <Checkbox
-        disabled={!row.original.canAddToCart}
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-        className={`translate-y-[2px] ${!row.original.canAddToCart?'bg-zinc-400':''}`}
-      />
-      )
+          disabled={!row.original.canAddToCart}
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+          className={`translate-y-[2px] ${
+            !row.original.canAddToCart ? "bg-zinc-400" : ""
+          }`}
+        />
+      );
     },
     enableSorting: false,
     enableHiding: false,
@@ -59,10 +62,14 @@ export const columns: TColumn<OrderItem,unknown>[] = [
     ),
     cell: ({ row }) => {
       return (
-        <Link target="_blank" href={`https://admin.shopify.com/store/aot-supply/orders/${row.original.id}`} className="underline underline-offset-4">
+        <Link
+          target="_blank"
+          href={`https://admin.shopify.com/store/aot-supply/orders/${row.original.id}`}
+          className="underline underline-offset-4"
+        >
           {row.original.name}
         </Link>
-      )
+      );
     },
     enableSorting: false,
   },
@@ -83,10 +90,10 @@ export const columns: TColumn<OrderItem,unknown>[] = [
       <DataTableColumnHeader column={column} title="Created At" />
     ),
     cell: ({ row }) => {
-      return format(new Date(row.original.created_at), "MMM dd, yyyy")
+      return format(new Date(row.original.created_at), "MMM dd, yyyy");
     },
     filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id))
+      return value.includes(row.getValue(id));
     },
   },
   {
@@ -96,11 +103,11 @@ export const columns: TColumn<OrderItem,unknown>[] = [
       <DataTableColumnHeader column={column} title="Total" />
     ),
     cell: ({ row }) => {
-      return row.original.total_price
+      return row.original.total_price;
     },
     enableSorting: false,
     filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id))
+      return value.includes(row.getValue(id));
     },
   },
   {
@@ -110,38 +117,58 @@ export const columns: TColumn<OrderItem,unknown>[] = [
       <DataTableColumnHeader column={column} title="Items" />
     ),
     cell: ({ row }) => {
-      const skuStatus = row.original.skuStatus ||{}
-      
-      return(
+      const skuStatus = row.original.skuStatus || {};
+      const skuData = row.original.skuData || {};
+      return (
         <>
-          <h3 className="text-red-500">Total:{row.original.line_items.length} items</h3>
-          {
-            row.original.line_items.map((item:any)=>{
-              let className=""
-              if(skuStatus[item.sku]=='Out'){
-                className+=" line-through bg-sky-200"
-              }
-              if (skuStatus[item.sku]=='Y'){
-                className+=" bg-yellow-300"
-              }
-              let url= `https://www.plamod.com/management#products?s=${item.sku}`
-              //如果sku是一个uuid
-              if(item.sku?.length==36){
-                url=`https://www.universaldist.com/item/detail/${item.sku}`
-              }
-              return(
-                <div key={item.sku} className={'flex items-center gap-2'}>
-                  <p className={className}>
-                    <Link href={url} target="_blank">{item.sku}x{item.quantity}</Link>
-                  </p>
-                  <p className="text-xs text-green-600">{row.original.products?.[item.sku]}</p>
-                </div>
-              )
-            })
-          
-          }
+          <h3 className="text-red-500">
+            Total:{row.original.line_items.length} items
+          </h3>
+          {row.original.line_items.map((item: any) => {
+            let className = "";
+            const needCount = skuData[item.sku]?.["need"] || 0;
+            const cartCount = skuData[item.sku]?.["cart"] || 0;
+            if (skuStatus[item.sku] == "Out") {
+              //已经下架
+              className += " line-through bg-sky-200";
+            }
+            //if (skuStatus[item.sku]=='Y'){
+            if (needCount > cartCount) {
+              className += " bg-yellow-300";
+            }
+            let url = `https://www.plamod.com/management#products?s=${item.sku}`;
+            //如果sku是一个uuid
+            if (item.sku?.length == 36) {
+              url = `https://www.universaldist.com/item/detail/${item.sku}`;
+            }
+            let numberClassName = `flex justify-center items-center font-bold leading-none w-4 h-4 rounded-full text-white text-xs`
+
+            return (
+              <div key={item.sku} className={"flex items-center gap-2"}>
+                {needCount > cartCount && (
+                  <>
+                    <div className={`bg-green-600 ${numberClassName}`}>
+                      {cartCount}
+                    </div>
+                    <div className={`bg-red-500 ${numberClassName}`}>
+                      {needCount}
+                    </div>
+                  </>
+                )}
+
+                <p className={className}>
+                  <Link href={url} target="_blank">
+                    {item.sku}x{item.quantity}
+                  </Link>
+                </p>
+                <p className="text-xs text-green-600">
+                  {row.original.products?.[item.sku]}
+                </p>
+              </div>
+            );
+          })}
         </>
-      ) 
+      );
     },
     enableSorting: false,
   },
@@ -152,13 +179,13 @@ export const columns: TColumn<OrderItem,unknown>[] = [
       <DataTableColumnHeader column={column} title="Destination" />
     ),
     cell: ({ row }) => {
-      const address=row.original.shipping_address
-      if (!address) return ''
-      return `${address.city},${address.province}, ${address.country}`
+      const address = row.original.shipping_address;
+      if (!address) return "";
+      return `${address.city},${address.province}, ${address.country}`;
     },
     enableSorting: false,
     filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id))
+      return value.includes(row.getValue(id));
     },
   },
-]
+];
--- app/(dashboard)/orders/page.tsx
+++ app/(dashboard)/orders/page.tsx
@@ -1,101 +1,128 @@
-"use client"
-import DataTable, { TableRef } from "@/components/biz/data-table"
-import { OrderItem, columns } from "./components/columns"
-import { orders, addToCart } from "@/apis/order"
-import Page from "@/components/biz/page"
-import CustomCard from "@/components/biz/custom-card"
-import { Button } from "@/components/ui/button"
-import { useRouter } from "next/navigation"
-import { addDays, format, set } from "date-fns"
-import { useQuery } from "@tanstack/react-query"
-import { DatePickerWithRange } from "@/components/ui/date-range-picker"
-import { Suspense, useRef, useState } from "react"
-import { DateRange } from "react-day-picker"
-import { useToast } from "@/components/ui/use-toast"
+"use client";
+import DataTable, { TableRef } from "@/components/biz/data-table";
+import { OrderItem, columns } from "./components/columns";
+import { orders, addToCart } from "@/apis/order";
+import Page from "@/components/biz/page";
+import CustomCard from "@/components/biz/custom-card";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { addDays, format, set } from "date-fns";
+import { useQuery } from "@tanstack/react-query";
+import { DatePickerWithRange } from "@/components/ui/date-range-picker";
+import { Suspense, useRef, useState } from "react";
+import { DateRange } from "react-day-picker";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function Orders() {
-  const router = useRouter()
-  const { toast } = useToast()
-  const [submitting, setSubmitting] = useState(false)
-  const tableRef = useRef<TableRef>(null)
+  const router = useRouter();
+  const { toast } = useToast();
+  const [submitting, setSubmitting] = useState(false);
+  const tableRef = useRef<TableRef>(null);
   const [date, setDate] = useState<DateRange | undefined>({
     from: addDays(new Date(), -7),
     to: new Date(),
-  })
-  const [variables, setVariables] = useState({})
+  });
+  const [variables, setVariables] = useState({});
   const { data, isLoading, refetch } = useQuery<any>({
     queryKey: ["orders", variables],
     queryFn: () => orders(variables),
     staleTime: 0,
-  })
+  });
   const handleAddToCart = () => {
-    const selection = tableRef.current?.rowSelection() || {}
-    const orderIds: string[] = Object.keys(selection)
+    const selection = tableRef.current?.rowSelection() || {};
+    const orderIds: string[] = Object.keys(selection);
     if (!orderIds.length) {
       toast({
         variant: "destructive",
-        title: 'Error',
-        description: 'Please select at least one order',
-      })
-      return
+        title: "Error",
+        description: "Please select at least one order",
+      });
+      return;
     }
-    setSubmitting(true)
-    addToCart(orderIds).then(() => {
-      tableRef.current?.clearSelection()
-      refetch()
-    }).finally(() => {
-      setSubmitting(false)
-    })
-  }
+    setSubmitting(true);
+    addToCart(orderIds)
+      .then(() => {
+        tableRef.current?.clearSelection();
+        refetch();
+      })
+      .finally(() => {
+        setSubmitting(false);
+      });
+  };
   const toolbar = (
     <div className="flex items-center gap-4">
-      <DatePickerWithRange value={date} onChange={(value) => {
-        setDate(value)
-      }} />
-      <Button type="button" loading={isLoading} size="sm" onClick={() => {
-        if (!date || (!date.from && !date.to)) {
-          toast({
-            variant: "destructive",
-            title: 'Error',
-            description: 'Please select a date range',
-          })
-          return
-        }
-        const to = date.to || date.from
-        const from = date.from || date.to
-        setVariables({
-          ...variables,
-          startDate: format(from!, 'yyyy-MM-dd'),
-          endDate: format(to!, 'yyyy-MM-dd'),
-          ts: new Date().getTime()
-        })
-      }}>Filter Orders</Button>
+      <DatePickerWithRange
+        value={date}
+        onChange={(value) => {
+          setDate(value);
+        }}
+      />
+      <Button
+        type="button"
+        loading={isLoading}
+        size="sm"
+        onClick={() => {
+          if (!date || (!date.from && !date.to)) {
+            toast({
+              variant: "destructive",
+              title: "Error",
+              description: "Please select a date range",
+            });
+            return;
+          }
+          const to = date.to || date.from;
+          const from = date.from || date.to;
+          setVariables({
+            ...variables,
+            startDate: format(from!, "yyyy-MM-dd"),
+            endDate: format(to!, "yyyy-MM-dd"),
+            ts: new Date().getTime(),
+          });
+        }}
+      >
+        Filter Orders
+      </Button>
+      <div className="flex items-center gap-4">
+        <div className="bg-green-600 w-4 h-4 rounded-full"></div>
+        <span className="text-sm">购物车已有数量</span>
+        <div className="bg-red-500 w-4 h-4 rounded-full"></div>
+        <span className="text-sm">总共需要的数量</span>
+      </div>
     </div>
-  )
+  );
   return (
-    <Page title="Orders" className="max-w-full" headerButtons={
-      [
-        <Button type="button" loading={submitting} key={'submit'} size="sm" onClick={() => {
-          handleAddToCart()
-        }}>Add to cart</Button>
-      ]
-    }>
+    <Page
+      title="Orders"
+      className="max-w-full"
+      headerButtons={[
+        <Button
+          type="button"
+          loading={submitting}
+          key={"submit"}
+          size="sm"
+          onClick={() => {
+            handleAddToCart();
+          }}
+        >
+          Add to cart
+        </Button>,
+      ]}
+    >
       <CustomCard>
-      <Suspense>
-        <DataTable<OrderItem,unknown>
-          ref={tableRef}
-          queryKey="orders"
-          columns={columns}
-          pagination={false}
-          data={data?.orders || []}
-          toolbar={toolbar}
-          selection={
-            {
+        <Suspense>
+          <DataTable<OrderItem, unknown>
+            ref={tableRef}
+            queryKey="orders"
+            columns={columns}
+            pagination={false}
+            data={data?.orders || []}
+            toolbar={toolbar}
+            selection={{
               enabled: true,
-            }
-          } />
-      </Suspense>
+            }}
+          />
+        </Suspense>
       </CustomCard>
     </Page>
-  )
+  );
 }
